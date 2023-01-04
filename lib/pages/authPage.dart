022190a0650960c8990071cd77195d742ca603b9
cyrus <cyruswgad@gmail.com>
2023-01-04 16:57:49 +0800

    misc

--- lib/pages/authPage.dart
+++ lib/pages/authPage.dart
@@ -12,6 +12,45 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  Future showNotification() => showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+            insetPadding: const EdgeInsets.all(10),
+            backgroundColor: Colors.transparent,
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.purpleAccent,
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                  child: const Text(
+                    "Welcome to Financially!",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Positioned(
+                  top: -120,
+                  child: Image.asset(
+                    'assets/icon.png',
+                    width: 150,
+                    height: 150,
+                  ),
+                ),
+              ],
+            ));
+      });
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -33,7 +72,9 @@ class _AuthPageState extends State<AuthPage> {
           // ),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () async {
+                await showNotification();
+              },
               icon: const Icon(Icons.notifications_rounded),
               tooltip: 'Notifications',
             )
